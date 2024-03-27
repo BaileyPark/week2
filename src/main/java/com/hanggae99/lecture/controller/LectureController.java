45ae352c6fe6ee66acc0ea38778d153edb044bb0
bailey <baileypark@kakao.com>
2024-03-27 22:23:53 +0900

    updated

--- src/main/java/com/hanggae99/lecture/controller/LectureController.java
+++ src/main/java/com/hanggae99/lecture/controller/LectureController.java
@@ -1,24 +1,25 @@
 package com.hanggae99.lecture.controller;
 
+import com.hanggae99.lecture.service.LectureService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/lectrue")
 public class LectureController {
 
-    @GetMapping("/apply")
-    public String specialLecture() {
+    LectureService lectureService = new LectureService();
+
+    @RequestMapping("/lectrue")
+    String specialLecture() {
         return "특강 신청받습니다.";
     }
 
-    @GetMapping("/{id}")
-//    public int point(@PathVariable int id) {
-//    public int point(@PathVariable(required = true) int id) {
-    public int point(@PathVariable ("id") int id) {
-        return id;
+    @GetMapping("/apply/{id}")
+    int apply(@PathVariable("id") int id) {
+        return lectureService.return_data(id);
+//        System.out.println(id);
     }
 
 }
---
+++ src/main/java/com/hanggae99/lecture/service/LectureService.java
@@ -0,0 +1,7 @@
+package com.hanggae99.lecture.service;
+
+public class LectureService {
+    public int return_data(int id) {
+        return id;
+    }
+}
